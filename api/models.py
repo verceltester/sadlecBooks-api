aa70cc4a1b18d6dbe26b4688e37f933070197ba2
verceltester <132142025+verceltester@users.noreply.github.com>
2023-09-26 22:43:56 +0530

    Update models.py from Ubuntu

--- api/models.py
+++ api/models.py
@@ -11,18 +11,22 @@ class Books(models.Model):
     imagelink = models.CharField(max_length = 300, null=True, blank=True)
     aurokart = models.CharField(max_length = 300, null=True, blank=True)
     otherinfo = models.TextField(max_length=150000, null=True, blank=True)
-    
+    searchableFields = ["bookTitle"]
+   
     class Meta:
         ordering = ['id']
 
     def __str__(self):
         return self.bookTitle
 
+
 class Chapter(models.Model):
     chapTitle = models.CharField(max_length=200, null=True, blank=True )
     chapText = models.TextField(max_length=250000, null=True, blank=True)
     Books = models.ForeignKey(Books, related_name='chap', on_delete=models.CASCADE)
     hastext = models.BooleanField(default=True)
+    FilterFields = ["Books", "hastext"]
+    searchableFields = ["chapTitle"]
 
     class Meta:
         ordering = ['id']
@@ -37,7 +41,8 @@ class Subhead1(models.Model):
     subhead1Text = models.TextField(max_length=250000, null=True, blank=True)
     Chapter = models.ForeignKey(Chapter, related_name='sub1', on_delete=models.CASCADE)
     hastext = models.BooleanField(default=True)
-
+    searchableFields = ["subhead1Titles"]
+    FilterFields = ["hastext"]
     class Meta:
         ordering = ['id']
 
@@ -50,11 +55,11 @@ class Subhead2(models.Model):
     subhead2Text = models.TextField(max_length=250000, null=True, blank=True)
     Subhead1 = models.ForeignKey(Subhead1, related_name='sub2',on_delete=models.CASCADE)
     hastext = models.BooleanField(default=True)
+    searchableFields = ["subhead2Titles"]
+    FilterFields = ["hastext"]
 
     class Meta:
         ordering = ['id']
 
     def __str__(self):
         return self.subhead2Titles
-
-
